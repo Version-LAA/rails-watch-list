91df3e38f57bc9b050e2a2bb633d06311ace48b8
Latoya Alford <lat.jamison@gmail.com>
2023-04-19 11:19:54 -0400

    implemented post method of creating new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,4 +8,15 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
