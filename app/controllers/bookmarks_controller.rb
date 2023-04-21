2030e56b6117f757153fed2ce6736409ec124697
Latoya Alford <lat.jamison@gmail.com>
2023-04-20 23:16:18 -0400

    added front end styling

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,19 @@
 class BookmarksController < ApplicationController
 
+  def index
+    @movies = Movie.where("title ILIKE ?", "%#{params[:query]}%")
+  end
   def new
     @movies = Movie.where("title ILIKE ?", "%#{params[:query]}%")
+  end
+
+  def create
+    @bookmark = Bookmark.new()
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :lists, only: [:index, :create, :new, :show] do
-    resources :bookmarks, only: [:create,:new]
+    resources :bookmarks, only: [:index,:create,:new]
   end
 end
