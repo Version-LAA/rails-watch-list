3eaab31961fa4c7c5e69a67f25d5937e2440b09b
Latoya Alford <lat.jamison@gmail.com>
2023-04-22 15:33:14 -0400

    fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  resources :lists, only: [:index, :create, :new, :show] do
+  root "lists#index"
+  resources :lists, only: [:create, :new, :show] do
     resources :bookmarks, only: [:index,:create,:new]
   end
 end
